ea22a40f7c1980e28f81ac73126b2a98a8e4891e
Danilo Aguiar <dansilvaagr@hotmail.com>
2023-06-07 09:52:01 -0300

    Algumas modificações feitas na forma em que o Menu é chamado

--- protocol-01.1/src/menu/HeaderMenu.js
+++ protocol-01.1/src/menu/HeaderMenu.js
@@ -1,28 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import Menu from './Menu';
+import ResponsiveMenu from './ResponsiveMenu';
 import TopHeader from './topHeader';
+import styles from '../css/HeaderMenu.module.css';
 
 const HeaderMenu = () => {
   const [isTop, setIsTop] = useState(true);
-  
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
   useEffect(() => {
     const handleScroll = () => {
       const show = window.scrollY < 100;
       if (show !== isTop) {
         setIsTop(show);
       }
-    }
-
-    document.addEventListener('scroll', handleScroll);
-    return () => {
-      document.removeEventListener('scroll', handleScroll);
     };
-  }, [isTop]);
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div>
-      <TopHeader isTop={isTop} />
-      <Menu isTop={!isTop} />
+      {windowWidth <= 768 ? (
+        <div>
+          <TopHeader isTop={isTop} />
+          <ResponsiveMenu isTop={isTop} />
+        </div>
+      ) : (
+        <div>
+          <Menu />
+        </div>
+      )}
     </div>
   );
 };
